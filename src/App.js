3175c4da7c045ffcddeb492979c59be173611b98
Weili Yi <yi@weiliy.net>
2017-10-06 23:33:15 +0800

    Get books from BooksAPI

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import SearchBooks from './SearchBooks.js';
 import ListBooks from './ListBooks';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
 
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then(books => {
+        this.setState({books});
+        console.log(books);
+      });
+  }
+
   render() {
+    const { books } = this.state;
+
     return (
       <div className="app">
         <Route exact path="/search" component={SearchBooks} />
-        <Route exact path="/" component={ListBooks} />
+        <Route exact path="/" render={() => (
+          <ListBooks books={books} />
+        )} />
       </div>
     )
   }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,46 +1,5 @@
 import React from 'react';
 
-const currentlyReading = [
-  {
-    title: 'To Kill a Mockingbird',
-    authors: 'Harper Lee',
-    cover: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-  }, {
-    title: 'Ender\'s Game',
-    authors: 'Orson Scott Card',
-    cover: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
-  }
-]
-
-const wantToRead = [
-  {
-    title: '1776',
-    authors: 'David McCullough',
-    cover: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
-  }, {
-
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    authors: 'J.K. Rowling',
-    cover: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api")',
-  }
-]
-const read = [
-  {
-    title: 'The Hobbit',
-    authors: 'J.R.R. Tolkien',
-    cover: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")',
-  }, {
-    title: 'Oh, the Places You\'ll Go!',
-    authors: 'Seuss',
-    cover: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")',
-  }, {
-    title: 'The Adventures of Tom Sawyer',
-    authors: 'Mark Twain',
-    cover: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")',
-  }
-]
-
-
 function Bookshelf(props) {
   const { title, books } = props;
 
@@ -53,8 +12,8 @@ function Bookshelf(props) {
             <li key={book.title}>
               <Book
                 title={book.title}
-                authors={book.authors}
-                cover={book.cover}
+                authors={book.authors.join(', ')}
+                cover={book.cover || `url(${book.imageLinks.thumbnail}`}
               />
             </li>
           )))}
@@ -95,6 +54,8 @@ function Book(props) {
 }
 
 function ListBooks(props) {
+  const { books } = props;
+
   return (
     <div className="list-books">
       <div className="list-books-title">
@@ -104,15 +65,15 @@ function ListBooks(props) {
         <div>
           <Bookshelf
             title="Currently Reading"
-            books={currentlyReading}
+            books={books.filter(b => b.shelf === 'currentlyReading')}
           />
           <Bookshelf
             title="Want to Read"
-            books={wantToRead}
+            books={books.filter(b => b.shelf === 'wantToRead')}
           />
           <Bookshelf
             title="Read"
-            books={read}
+            books={books.filter(b => b.shelf === 'read')}
           />
         </div>
       </div>
